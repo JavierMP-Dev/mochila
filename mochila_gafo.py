264cf2b21956919a7eb088964b6ccc53a9799ea4
javier montoya <montoya.javier.15@hotmail.com>
2024-06-25 09:49:06 -0600

    resolviendo el problema de la mochila con programacion dinamica

--- mochila_gafo.py
+++ mochila_gafo.py
@@ -1,72 +1,42 @@
-import networkx as nx  #libreria para analizar grafos, sirve para implementar bfs o dfs
-import matplotlib.pyplot as plt  #crea visualizaciones estaticas de los grafos
+import time
 
-def mochila_backtracking(values, weights, max_weight):
-    # Número de elementos
+def knapsack(values, weights, max_weight):
     n = len(values)
+    # Crear una tabla para almacenar los valores máximos para cada subproblema
+    dp = [[0 for _ in range(max_weight + 1)] for _ in range(n + 1)]
     
-    # Función auxiliar de DFS y backtracking
-    def dfs(index, current_weight, current_value, taken_items, G, parent=None):
-        # Añadir nodo al grafo
-        node = (index, current_weight, current_value)
-        G.add_node(node)
-        if parent:
-            G.add_edge(parent, node)
-        
-        # si hemos considerado todos los elementos se retorna el valor y los elementos tomados
-        if index == n:
-            return current_value, taken_items
-        
-        # Opción 1: no tomar el elemento actual
-        max_value, best_items = dfs(index + 1, current_weight, current_value, taken_items, G, node)
-        
-        # Opción 2: tomar el elemento actual (si cabe en la mochila)
-        if current_weight + weights[index] <= max_weight:
-            value_with_item, items_with_item = dfs(index + 1, current_weight + weights[index], current_value + values[index], taken_items + [index], G, node)
-            # Comparamos si tomar el elemento actual da una mejor solución
-            if value_with_item > max_value:
-                max_value = value_with_item
-                best_items = items_with_item
-        
-        return max_value, best_items #retorna el valor maximo y los puestos ideales
+    # Llenar la tabla de manera bottom-up
+    for i in range(1, n + 1):
+        for w in range(max_weight + 1):
+            if weights[i - 1] <= w:
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1])
+            else:
+                dp[i][w] = dp[i - 1][w]
     
-    # Crear grafo vacío
-    G = nx.DiGraph()
+    # Imprimir la tabla
+    for row in dp:
+        print(row)
     
-    # Llamamos a la función DFS desde el primer elemento
-    max_value, best_items = dfs(0, 0, 0, [], G)
+    # Encontrar los elementos que se tomaron para obtener el resultado óptimo
+    taken_items = []
+    w = max_weight
+    for i in range(n, 0, -1):
+        if dp[i][w] != dp[i - 1][w]:
+            taken_items.append(i - 1)
+            w -= weights[i - 1]
     
-    # Regresamos el máximo valor, los elementos tomados y el grafo
-    return max_value, best_items, G
+    # El valor máximo está en dp[n][max_weight]
+    return dp[n][max_weight], taken_items
 
 # Ejemplo de uso
-values = [60, 100, 120, 80]
-weights = [10, 20, 30, 15]
+values = [60, 100, 120]
+weights = [10, 20, 30]
 max_weight = 50
 
-max_value, best_items, G = mochila_backtracking(values, weights, max_weight)
+start_time = time.time()
+max_value, taken_items = knapsack(values, weights, max_weight)
+end_time = time.time()
+
 print("El valor máximo que se puede obtener es:", max_value)
-print("Los elementos a tomar son:", best_items)
-
-# Dibujar el grafo
-pos = nx.spring_layout(G)
-labels = {(i, w, v): f"{i},{w},{v}" for i, w, v in G.nodes()}
-
-#funcion para colorear los nodos
-nx.draw(G, pos, with_labels=True, labels=labels, node_size=700, node_color="skyblue", font_size=8, font_weight="bold", arrows=True)
-
-# funcion para Resaltar la solución óptima
-path = [(0, 0, 0)]
-current_weight = 0
-current_value = 0
-for i in best_items:
-    current_weight += weights[i]
-    current_value += values[i]
-    path.append((i + 1, current_weight, current_value))
-
-path_edges = [(path[i], path[i+1]) for i in range(len(path) - 1)]
-nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color="pink", width=2)
-
-plt.title("Grafo de decisiones del problema de la mochila")
-plt.show()
-#este programa aun no funciona
+print("Los índices de los elementos a tomar son:", taken_items)
+print("Tiempo de ejecución:", end_time - start_time, "segundos")
